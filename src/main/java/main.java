414b9164e673633863bb3f75ece3f1c570839c80
LuisMiguelBr <luismiguelbravolopez3@gmail.com>
2022-04-19 11:00:13 +0200

    cuarto commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,32 +9,37 @@ public class main {
         pedir(anum);
 
         comparar(anum,anum2);
-
+        imprimir(anum,anum2);
 
     }
 
     private static void pedir(int anum[]){
         Scanner sc = new Scanner(System.in);
-        for(int i=0; i<=10; i++){
+        for(int i=0; i<10; i++){
             System.out.println("Numero:");
             anum[i]=Integer.parseInt(sc.nextLine());
         }
     }
     private  static void comparar(int anum[],int anum2[]){
         int cont=0;
-        System.out.println("{");
-        for(int x=0; x<=10; x++){
-            anum2[x]=anum[x];
-            for(int y=0; y<=10; y++){
-                if(anum2[x] == anum[y]);
-                cont++;
+        for(int x=0; x<10; x++){
+            cont=0;
+            for(int y=0; y<10; y++){
+                if(anum[x] == anum[y])
+                anum2[x]=cont++;
             }
-            System.out.printf("%d = %d, ",anum2[x],cont);
         }
-        System.out.println("}");
-
     }
-
+    private  static void imprimir(int anum[],int anum2[]){
+        System.out.printf("\n{");
+        for(int x=0; x<10; x++){
+            if(x==9)
+                System.out.printf("%d=%d",anum[x],anum2[x]);
+            else
+                System.out.printf("%d=%d,",anum[x],anum2[x]);
+        }
+        System.out.printf("}");
+    }
 
 }
 

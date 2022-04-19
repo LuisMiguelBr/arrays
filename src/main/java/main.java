4469bec63400bc777e3814ec3e76c86b00fe27cd
LuisMiguelBr <luismiguelbravolopez3@gmail.com>
2022-04-19 10:16:22 +0200

    Segundo commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,20 +4,35 @@ public class main {
 
         Scanner sc = new Scanner(System.in);
         int [] anum = new int[10];
-        int [] anum2 = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int [] anum2 = new int[10];
+
+        pedir(anum);
+
+        comparar(anum,anum2);
 
 
+    }
+
+    private static void pedir(int anum[]){
+        Scanner sc = new Scanner(System.in);
         for(int i=0; i<=10; i++){
             System.out.println("Numero:");
             anum[i]=Integer.parseInt(sc.nextLine());
         }
-        for(int y=0; y<=10; y++){
-            System.out.printf("%d - %d",anum[y],anum2[y]);
+    }
+    private  static void comparar(int anum[],int anum2[]){
+        int cont=0;
+        for(int x=0; x<=10; x++){
+            anum2[x]=anum[x];
+            for(int y=0; y<=10; y++){
+                if(anum2[x] == anum[y]);
+                cont++;
+            }
+
         }
 
+
     }
-
-
 
 
 }
